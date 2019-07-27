ddeeba83d9eea5d942423991e7a761d8261bf332
Lantao Cui <clt2738461982@gmail.com>
2019-07-27 16:57:45 -0400

    playe_move

---
+++ Assets/Scripts/Player_move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigid;
+    private Vector3 change;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero){
+            Character_move();
+            animator.SetFloat("MOVE_X", change.x);
+            animator.SetFloat("MOVE_Y", change.y);
+            animator.SetBool("IS_WALKING", true);
+        }
+        else
+        {
+            animator.SetBool("IS_WALKING", false);
+        }
+    }
+
+    //Script to make the character move
+    void Character_move()
+    {
+        rigid.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
